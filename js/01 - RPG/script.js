22c078d299d9e084d5966e5d5add6659fac071c5
Leila Zrim <leilazrim@gmail.com>
2023-12-21 13:50:19 +0100

    [Challenge 57] Added the "button functions" property with values - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -19,6 +19,7 @@ const locations = [
   {
     name: "town square",
     "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
   },
 ];
 
