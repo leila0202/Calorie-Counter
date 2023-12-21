1d3ea5e10a6a0291292535e12a766f7223975aa1
Leila Zrim <leilazrim@gmail.com>
2023-12-21 14:45:08 +0100

    [Challenge 68] Use object for "text.innerText" - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -46,8 +46,7 @@ function update(location) {
   button2.onclick = location["button functions"][1];
   button3.innerText = location["button text"][2];
   button3.onclick = location["button functions"][2];
-  text.innerText =
-    'You are in the town square. You see a sign that says "Store".';
+  text.innerText = location.text;
 }
 function goTown() {
   update(locations[0]);
