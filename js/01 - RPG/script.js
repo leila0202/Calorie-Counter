d2f4df62b8b1652c1d8afee4c0083fff7c2bbd0e
Leila Zrim <leilazrim@gmail.com>
2023-12-21 15:49:15 +0100

    [Challenge 91] Display the name of the new weapon to the user - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -105,7 +105,7 @@ function buyWeapon() {
     currentWeapon++;
     goldText.innerText = gold;
     let newWeapon = weapons[currentWeapon].name;
-    text.innerText = "You now have a new weapon.";
+    text.innerText = "You now have a " + newWeapon + ".";
   }
 }
 
