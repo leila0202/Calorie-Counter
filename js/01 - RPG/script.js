0f51b07a2578038614197478b28e12d0b3e5d1aa
Leila Zrim <leilazrim@gmail.com>
2023-12-21 13:37:16 +0100

    [Challenge 52] Changed variable to const - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -15,7 +15,7 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-let locations = [];
+const locations = [];
 
 //initialize buttons
 button1.onclick = goStore;
