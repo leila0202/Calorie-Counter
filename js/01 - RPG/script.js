8719de4cb47856be5438bcc286cab132af01e678
Leila Zrim <leilazrim@gmail.com>
2023-12-22 14:14:48 +0100

    [Challenge 125] Check if player health is less or equal to 0 - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -184,6 +184,9 @@ function attack() {
     weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  }
 }
 
 function dodge() {}
