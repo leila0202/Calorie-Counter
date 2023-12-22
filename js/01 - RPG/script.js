846b4d85b071028298905671b8fde13eb426811f
Leila Zrim <leilazrim@gmail.com>
2023-12-22 10:49:05 +0100

    [Challenge 108] Inform the player to not sell his last weapon - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -126,6 +126,8 @@ function sellWeapon() {
     let currentWeapon = inventory.shift();
     text.innerText = "You sold a " + currentWeapon + ".";
     text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "Don't sell your only weapon!";
   }
 }
 
