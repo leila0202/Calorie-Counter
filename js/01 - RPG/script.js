8818206dc144ad6a74a85ac5011e8f434371d314
Leila Zrim <leilazrim@gmail.com>
2023-12-22 10:46:36 +0100

    [Challenge 107] Added showing the current inventory after selling weapons - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -125,6 +125,7 @@ function sellWeapon() {
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
     text.innerText = "You sold a " + currentWeapon + ".";
+    text.innerText += " In your inventory you have: " + inventory;
   }
 }
 
