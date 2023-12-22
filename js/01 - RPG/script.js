c659e04f74fdc152d006261c0b3f5e97af027187
Leila Zrim <leilazrim@gmail.com>
2023-12-22 11:22:05 +0100

    [Challenge 117] show the monster stats if we fight a monster - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -169,6 +169,7 @@ function fightDragon() {
 function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
 }
 
 function attack() {}
