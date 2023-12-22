0f0328215fd129f8b3d3c68abe6d0954c3296d5e
Leila Zrim <leilazrim@gmail.com>
2023-12-22 09:56:08 +0100

    [Challenge 105] Remove first element in the inventory and asign it to "currentWeapon" - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -123,7 +123,7 @@ function sellWeapon() {
   if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
-    let currentWeapon;
+    let currentWeapon = inventory.shift();
   }
 }
 
