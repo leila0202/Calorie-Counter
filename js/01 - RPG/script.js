2b13e3eec635d61c3cf6bc7d53d7a9b52bdd8f50
Leila Zrim <leilazrim@gmail.com>
2023-12-22 15:06:29 +0100

    [Challenge 138] Added "lose" location - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -90,6 +90,12 @@ const locations = [
     "button functions": [goTown, goTown, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.',
   },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button functions": [restart, restart, restart],
+    text: "You die. ☠️",
+  },
 ];
 
 //initialize buttons
