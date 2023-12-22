8e4bd4f9cbbe01fa1e9f7c9858b5f2f5ffde7499
Leila Zrim <leilazrim@gmail.com>
2023-12-22 15:04:01 +0100

    [Challenge 137] Add restart function - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -217,3 +217,15 @@ function defeatMonster() {
 function lose() {
   update(locations[5]);
 }
+
+function restart() {
+  xp = 0;
+  health = 100;
+  gold = 50;
+  currentWeapon = 0;
+  inventory = ["stick"];
+  xpText.innerText = xp;
+  healthText.innerText = health;
+  goldText.innerText = gold;
+  goTown();
+}
