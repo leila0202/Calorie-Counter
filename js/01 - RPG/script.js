06bdf7f3983dd14a54e581835737d4af7f742539
Leila Zrim <leilazrim@gmail.com>
2023-12-22 13:34:00 +0100

    [Challenge 118] Reflect monster name and health in the monster stats - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -170,6 +170,8 @@ function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {}
