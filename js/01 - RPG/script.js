be72bacbec8dfc727937d23ed017d90865560491
Leila Zrim <leilazrim@gmail.com>
2023-12-22 13:58:00 +0100

    [Challenge 121] Reduce Player health by monster lvl - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -179,6 +179,7 @@ function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText +=
     " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
 }
 
 function dodge() {}
