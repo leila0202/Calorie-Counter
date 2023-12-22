3da6f2a048df0d0101749ea7f20351e0dcc7dcea
Leila Zrim <leilazrim@gmail.com>
2023-12-22 14:08:15 +0100

    [Challenge 123] Add a random value based on the player xp to the attack power - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -180,7 +180,8 @@ function attack() {
   text.innerText +=
     " You attack it with your " + weapons[currentWeapon].name + ".";
   health -= monsters[fighting].level;
-  monsterHealth -= weapons[currentWeapon].power;
+  monsterHealth -=
+    weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
 }
 
 function dodge() {}
