a3b7598222014bb4d56492ae0e2f1dd790fe3309
Leila Zrim <leilazrim@gmail.com>
2023-12-27 15:26:51 +0100

    [Challenge 157] Last weapon should not break - finished

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -216,7 +216,7 @@ function attack() {
   } else if (monsterHealth <= 0) {
     fighting === 2 ? winGame() : defeatMonster();
   }
-  if (Math.random() <= 0.1) {
+  if (Math.random() <= 0.1 && inventory.length !== 1) {
     text.innerText += " Your " + inventory.pop() + " breaks.";
     currentWeapon--;
   }
