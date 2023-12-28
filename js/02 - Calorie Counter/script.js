cf90b4c433a2d2f297e31360c6fb0df7fd2ebedd
Leila Zrim <leilazrim@gmail.com>
2023-12-28 14:39:41 +0100

    [js - 02][Step 44] Add value to the "HTMLString"

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -22,5 +22,6 @@ function addEntry() {
   );
   const entryNumber =
     targetInputContainer.querySelectorAll('input[type="text"]').length;
-  const HTMLString = ``;
+  const HTMLString = `
+  <label>Entry ${entryNumber} Name</label>`;
 }
