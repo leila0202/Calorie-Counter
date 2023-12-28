2c4044af2700adcf67dc4b81a9ae5ae44fd7e3fa
Leila Zrim <leilazrim@gmail.com>
2023-12-28 13:58:41 +0100

    [js - 02][Step 38] get the targetInputcontainer correspondig to the "targetId"

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -18,4 +18,7 @@ function isInvalidInput(str) {
 
 function addEntry() {
   const targetId = "#" + entryDropdown.value;
+  const targetInputContainer = document.querySelector(
+    targetId + " .input-container"
+  );
 }
