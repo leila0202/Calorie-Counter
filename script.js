a14f6cb0db75208f504b54619e9be831bc61888b
Leila Zrim <leilazrim@gmail.com>
2023-12-28 14:59:03 +0100

    [js - 02][Step 47] Added another "label" to the "HTMLString"

--- script.js
+++ script.js
@@ -24,5 +24,6 @@ function addEntry() {
     targetInputContainer.querySelectorAll('input[type="text"]').length;
   const HTMLString = `
   <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>`;
+  <input type="text" placeholder="Name" id="${entryDropdown.value}-${entryNumber}-name"></input>
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>`;
 }
