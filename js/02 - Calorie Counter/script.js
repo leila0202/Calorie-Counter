8c95d6cc104ed88d166e2123d4584c110c4b2d11
Leila Zrim <leilazrim@gmail.com>
2023-12-28 11:13:52 +0100

    [js - 02][Step 18] Add "cleanInputString" function without functionality for now

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -5,3 +5,5 @@ const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
+
+function cleanInputString(str) {}
