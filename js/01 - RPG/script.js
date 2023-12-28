37b6655967363f90030c78498ea9f80f11a9a5ed
Leila Zrim <leilazrim@gmail.com>
2023-12-28 08:46:43 +0100

    [Challenge 166] Added new line

--- js/01 - RPG/script.js
+++ js/01 - RPG/script.js
@@ -287,5 +287,5 @@ function pick(guess) {
   while (numbers.length < 10) {
     numbers.push(Math.floor(Math.random() * 11));
   }
-  text.innerText = "You picked " + guess + ". Here are the random numers:";
+  text.innerText = "You picked " + guess + ". Here are the random numers:\n";
 }
