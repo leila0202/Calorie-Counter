4160ea54844aa688eb8866e89663410a1e4fd291
Leila Zrim <leilazrim@gmail.com>
2023-12-28 14:20:17 +0100

    [js - 02][Step 40] Use template literal and remove "targetId"

--- script.js
+++ script.js
@@ -17,8 +17,7 @@ function isInvalidInput(str) {
 }
 
 function addEntry() {
-  const targetId = "#" + entryDropdown.value;
   const targetInputContainer = document.querySelector(
-    `${targetId} .input-container`
+    `#${entryDropdown.value} .input-container`
   );
 }
