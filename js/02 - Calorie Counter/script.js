ce6abab8651853651e69eed52ccf7211e649ed3e
Leila Zrim <leilazrim@gmail.com>
2023-12-29 10:16:10 +0100

    [js - 02][Step 61] Add popup displaying invalid input

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -36,6 +36,7 @@ function getCaloriesFromInputs(list) {
     const currVal = cleanInputString(list[i].value);
     const invalidInputMatch = isInvalidInput(currVal);
     if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
     }
   }
 }
