4723365b904cc39462ebd6b2e6845d2ec38f3c17
Leila Zrim <leilazrim@gmail.com>
2023-12-29 10:22:31 +0100

    [js - 02][Step 66] Prevent submit and reset "isError"

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -30,7 +30,10 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
-function calculateCalories(e) {}
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+}
 
 function getCaloriesFromInputs(list) {
   let calories = 0;
