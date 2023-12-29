7a9663c521dc7e469adb828f04394b9f3c84eabc
Leila Zrim <leilazrim@gmail.com>
2023-12-29 14:10:23 +0100

    [js - 02][Step 76] calculate remaining calories

--- script.js
+++ script.js
@@ -60,6 +60,8 @@ function calculateCalories(e) {
   }
   const consumedCalories =
     breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories =
+    budgetCalories - consumedCalories + exerciseCalories;
 }
 
 function getCaloriesFromInputs(list) {
