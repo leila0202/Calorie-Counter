13d67924d666250d7c6fcc1e03c9c21ee38df38e
Leila Zrim <leilazrim@gmail.com>
2023-12-29 10:11:18 +0100

    [js - 02][Step 58] Use "cleanInputString" to clean up unwanted chars in the input values

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -33,7 +33,7 @@ function addEntry() {
 function getCaloriesFromInputs(list) {
   let calories = 0;
   for (let i = 0; i < list.length; i++) {
-    const currVal = list[i].value;
+    const currVal = cleanInputString(list[i].value);
   }
 }
 
