196f1701c94e0c493baeada3af193d483c9ac010
Leila Zrim <leilazrim@gmail.com>
2023-12-29 14:20:55 +0100

    [js - 02][Step 81] Use Math.abs to only show positive values

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -63,7 +63,9 @@ function calculateCalories(e) {
   const remainingCalories =
     budgetCalories - consumedCalories + exerciseCalories;
   const surplusOrDeficit = remainingCalories >= 0 ? "Surplus" : "Deficit";
-  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${remainingCalories} Calorie ${surplusOrDeficit}</span>`;
+  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+    remainingCalories
+  )} Calorie ${surplusOrDeficit}</span>`;
 }
 
 function getCaloriesFromInputs(list) {
