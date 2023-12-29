8248cb546109d451b2469d20e6423bd73e930b76
Leila Zrim <leilazrim@gmail.com>
2023-12-29 10:12:23 +0100

    [js - 02][Step 59] Check for invalid Input

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function getCaloriesFromInputs(list) {
   let calories = 0;
   for (let i = 0; i < list.length; i++) {
     const currVal = cleanInputString(list[i].value);
+    const invalidInputMatch = isInvalidInput(currVal);
   }
 }
 
