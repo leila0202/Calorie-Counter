c875ccd341483adc4f0dd6e00483bf68a93702a6
Leila Zrim <leilazrim@gmail.com>
2023-12-29 10:06:09 +0100

    [js - 02][Step 54] Add empty "getCaloriesFromInputs" function

--- js/02 - Calorie Counter/script.js
+++ js/02 - Calorie Counter/script.js
@@ -30,4 +30,6 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
+function getCaloriesFromInputs(list) {}
+
 addEntryButton.addEventListener("click", addEntry);
